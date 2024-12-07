61af2b6e450b74cf02ac70d5bbcf859288b1152e
i001962@gmail.com <i001962@gmail.com>
2024-12-07 13:40:19 -0800

    roll back

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,24 +7,7 @@ const Demo = dynamic(() => import("~/components/Demo"), {
 });
 
 export default function App(
-  { title }: { title?: string } = { title: "EPL Match Summaries" }
+  { title }: { title?: string } = { title: "d33m EPL" }
 ) {
-  const backgroundImageUrl = "defifa_spinner.gif"; // Replace with your image URL
-  
-  return (
-    <div
-      style={{
-        backgroundImage: `url(${backgroundImageUrl})`,
-        backgroundSize: "cover", // Makes the image cover the whole area
-        backgroundPosition: "center", // Centers the image
-        minHeight: "100vh", // Makes sure the background image covers the full viewport height
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center", // Centers the content in the middle
-        padding: "20px", // Add padding to the content if needed
-      }}
-    >
-      <Demo title={title} />
-    </div>
-  );
+  return <Demo title={title} />;
 }
