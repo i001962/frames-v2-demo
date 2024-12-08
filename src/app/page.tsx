b3e5c55d6fc4312c7e6dd3dd0f4cb4d9943e5747
i001962@gmail.com <i001962@gmail.com>
2024-12-07 16:10:48 -0800

    more og tries

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,10 +7,10 @@ const frame = {
   version: "next",
   imageUrl: `${appUrl}/opengraph-image`,
   button: {
-    title: "See match summaries",
+    title: "Live match summaries",
     action: {
       type: "launch_frame",
-      name: "d33m EPL",
+      name: "EPL Match Summaries",
       url: appUrl,
       splashImageUrl: `${appUrl}/defifa_spinner.gif`,
       splashBackgroundColor: "#BD195D",
@@ -22,10 +22,10 @@ export const revalidate = 300;
 
 export async function generateMetadata(): Promise<Metadata> {
   return {
-    title: "d33m EPL",
+    title: "EPL Match Summaries",
     openGraph: {
-      title: "d33m EPL",
-      description: "d33m EPL",
+      title: "EPL Match Summaries",
+      description: "Get the live match summaries from the English Premier League",
     },
     other: {
       "fc:frame": JSON.stringify(frame),
