db5ca88b51eca4c2cdd3630c3b0fdae6ce25c7b3
i001962@gmail.com <i001962@gmail.com>
2024-12-15 11:44:10 -0800

    fixes fav team break bug

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -7,7 +7,7 @@ const Demo = dynamic(() => import("~/components/Demo"), {
 });
 
 export default function App(
-  { title }: { title?: string } = { title: "d33m" }
+  { title }: { title?: string } = { title: "FC Footy App" }
 ) {
   console.log(title); // TODO remove this shit
   return <Demo />;
