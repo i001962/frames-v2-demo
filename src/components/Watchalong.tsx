5a855fcd6c20733655f6825cd0e1447b586b3bd5
i001962@gmail.com <i001962@gmail.com>
2024-12-21 17:12:13 -0800

    fafo lab exp

--- src/components/Watchalong.tsx
+++ src/components/Watchalong.tsx
@@ -1,8 +1,10 @@
+import { Button } from "./ui/Button";
+
 const Watchalong = () => {
     return (
         <div className="text-center text-lg text-fontRed">
-            <div className="relative" style={{ paddingBottom: '56.25%' /* 16:9 aspect ratio */ }}>
-                <iframe
+            <div className="relative" style={{ height: '500px' }}>
+            {/* <iframe
                     className="absolute top-0 left-0 w-full h-full"
                     //src="https://www.youtube.com/embed/dQw4w9WgXcQ?si=ZTNYaWslQH8VCtsA"
                     src="https://webrtctzn.glitch.me/?room=footy"
@@ -11,7 +13,8 @@ const Watchalong = () => {
                     allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                     referrerPolicy="strict-origin-when-cross-origin"
                     allowFullScreen
-                ></iframe>
+                ></iframe> */}
+                <Button onClick={() => window.open('https://webrtctzn.glitch.me/?room=footy', '_blank')}>Join the watchalong</Button>
             </div>
         </div>
     );
